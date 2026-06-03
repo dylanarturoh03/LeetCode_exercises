16b9e1b2f65107171c5ed133a033cf2f984c3bd4
dylanarturoh03 <dylanarturoh@outlook.es>
2026-06-03 13:42:23 -0600

    Solve merge k lists with divide & conquer algorithm.

--- mergeKLists.py
+++ mergeKLists.py
@@ -32,7 +32,43 @@ class Solution:
 
         return dummy.next
 
+    def mergeKLists_divideConquer(
+        self,
+        lists: list[Optional[ListNode]]
+    ) -> Optional[ListNode]:
+        '''
+            Merge K-lists by divide & conquer array and merging both halves.
+        '''
+        def merge(list1: ListNode, list2: ListNode) -> Optional[ListNode]:
+            '''Merge two given ordered linked lists.'''
+            dummy = ListNode()
+            curr = dummy
+
+            while list1 and list2:
+                if list1.val <= list2.val:
+                    nxt = list1
+                    list1 = list1.next
+                else:
+                    nxt = list2
+                    list2 = list2.next
+
+                curr.next = nxt
+                curr = curr.next
+
+            rList = list1 if not list2 else list2
+
+            curr.next = rList
+            return dummy.next
+
+        if len(lists) <= 1:
+            return lists[0] if lists else None
+
+        mid = len(lists) // 2
+        fHalf = self.mergeKLists_divideConquer(lists[:mid])
+        sHalf = self.mergeKLists_divideConquer(lists[mid:])
+        return merge(fHalf, sHalf)
+
 
 sol = Solution()
 lists = [buildList([1, 2, 4]), buildList([1, 3, 5]), buildList([3, 6])]
-printList(sol.mergeKLists(lists))
+printList(sol.mergeKLists_divideConquer(lists))
