4a9416c458395c75b3569647ab9a3f1470a4f7e4
dylanarturoh03 <dylanarturoh@outlook.es>
2026-07-28 12:13:03 -0600

    Manually coded a min-heap + methods to efficiently get kth.

--- kthLargestInStream.py
+++ kthLargestInStream.py
@@ -1,28 +1,78 @@
 class KthLargest:
 
     def __init__(self, k: int, nums: list[int]):
-        '''Preprocess nums by ordering to make kth computation constant'''
-        nums.sort()
+        '''Preprocess nums to create a k size min-heap.'''
         self.k = k
-        self.nums = nums
+        self.nums = self._min_heapify(nums)
+        for _ in range(len(self.nums) - k):
+            self._pop()
 
     def add(self, val: int) -> int:
-        '''Add val to nums while maintaning ordering. Copmute and return kth'''
-        if not self.nums or val >= self.nums[-1]:
+        '''
+        Determine if val goes in heap,
+        then move it to correct pos if needed.
+        '''
+        if self.k > len(self.nums):
             self.nums.append(val)
+            self._sift_up()
+        elif self.nums[0] < val:
+            self.nums[0] = val
+            self._sift_down(self.nums)
 
-        else:
-            for i, n in enumerate(self.nums):
-                if n >= val:
-                    self.nums.insert(i, val)
-                    break
+        return self.nums[0]
 
-        return self.nums[len(self.nums) - self.k]
+    def _sift_down(self, arr: list[int], idx: int = 0) -> None:
+        '''Bubble-down given idx value to it's correct position.'''
+        curr = mn = idx
+
+        while True:
+            left = curr * 2 + 1
+            if len(arr) > left and arr[left] < arr[mn]:
+                mn = left
+
+            right = curr * 2 + 2
+            if len(arr) > right and arr[right] < arr[mn]:
+                mn = right
+
+            if mn == curr:
+                break
+
+            arr[mn], arr[curr] = arr[curr], arr[mn]
+            curr = mn
+
+    def _sift_up(self) -> None:
+        '''Bubble-up last element to it's correct position.'''
+        curr = len(self.nums) - 1
+
+        while curr > 0:
+            p = (curr - 1) // 2
+            if self.nums[p] <= self.nums[curr]:
+                break
+
+            self.nums[curr], self.nums[p] = self.nums[p], self.nums[curr]
+            curr = p
+
+    def _pop(self) -> int:
+        '''Pop root from min-heap.'''
+        self.nums[-1], self.nums[0] = self.nums[0], self.nums[-1]
+        removed = self.nums.pop()
+        self._sift_down(self.nums)
+
+        return removed
+
+    def _min_heapify(self, arr: list[int]) -> list[int]:
+        '''Enforce min-heap conditions to a given array.'''
+        for i in range((len(arr) - 2) // 2, -1, -1):
+            self._sift_down(arr, i)
+
+        return arr
 
 
-obj = KthLargest(1, [6])
-print(obj.add(3))
+obj = KthLargest(3, [1, 2, 3, 4])
+print(obj.add(0))
+print(obj.nums)
 print(obj.add(5))
 print(obj.add(6))
 print(obj.add(7))
 print(obj.add(8))
+print(obj.nums)
